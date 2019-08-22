841b016220adbc29ec99b82f2c1a82306c0a2e27
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-22 18:11:02 +0200

    feat: implement LocalizedErrorFactory

--- src/LocalizedErrorFactory.ts
+++ src/LocalizedErrorFactory.ts
@@ -1,2 +1,15 @@
+import { ChainedErrorFactory } from "typescript-chained-error";
+
+import { LocalizedError } from "./LocalizedError";
+
 export namespace LocalizedErrorFactory {
+    export function make<T extends Error>(error: T, localizedMessage: { [x: string]: string }): LocalizedError & T {
+        const localizedError: LocalizedError = {
+            message: error.message,
+            details: {
+                localizedMessage,
+            },
+        };
+        return ChainedErrorFactory.make(error, undefined, localizedError);
+    }
 }
